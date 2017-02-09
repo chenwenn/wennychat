098b2b70b958058307dc0dd973a8835faafe2e00
chenwenn <rainbor5320520@gmail.com>
2017-02-09 16:51:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -45,31 +45,11 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//全部訊息  case *linebot.TextMessage:
-
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
-
-			        case "你好":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(linebot.TextMessage+"可以跟我聊天哦!")).Do();
-				 case "晚安":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("晚安，祝你有好夢!")).Do();
-				 case "你是誰":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你的女朋友")).Do();
-				 case "今天吃甚麼":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("廁所用餐很適合你")).Do();
-				 case "蔡英文":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("他是歐巴森嗎?")).Do();
-				 case "給我錢":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("我會燒給你")).Do();
-				 case "我可以說什麼?":
-				 bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你可以說 你是誰 今天吃甚麼 給我錢 你好")).Do();
-		
-				default:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你可以說 你是誰 今天吃甚麼 給我錢 你好")).Do();
-		
-				 err != nil {
+			case *linebot.TextMessage:
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.ID+":"+message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
