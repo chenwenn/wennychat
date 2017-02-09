d12f9bc5e95deb485568e603870945faf36c51c7
chenwenn <rainbor5320520@gmail.com>
2017-02-09 16:54:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
-			case 你好:
+			case "你好":
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
