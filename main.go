7f0beb6b400aa457d90765d1d7bb0892921b9796
chenwenn <rainbor5320520@gmail.com>
2017-02-09 18:39:48 +0800

    Update main.go

--- main.go
+++ main.go
@@ -55,18 +55,55 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				
 				log.Println(message.Text)
 				if message.Text=="狗" {
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("1"+message.Text)).Do();	
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("豬隊友就是在說你吧!")).Do();	
 				err != nil {
 					log.Print(err)
 				}
 				}
 				if message.Text=="晚安" {
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("2"+message.Text)).Do();	
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("晚安!祝你有個好夢~")).Do();	
 				err != nil {
 					log.Print(err)
 				}
 				}
 				
+					if message.Text=="你好" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text+"!可以跟我聊天ㄛ!")).Do();	
+				err != nil {
+					log.Print(err)
+				}
+				}
+					if message.Text=="你是誰" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("FBI偵探"+messge.Texta)).Do();	
+				err != nil {
+					log.Print(err)
+				}
+				}
+						if message.Text=="蔡英文" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("他是歐巴森嗎")).Do();	
+				err != nil {
+					log.Print(err)
+				}
+				}
+					if message.Text=="今天吃甚麼" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("廁所用餐很適合你")).Do();	
+				err != nil {
+					log.Print(err)
+				}
+				}
+				if message.Text=="給我錢" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("我會燒給你")).Do();	
+				err != nil {
+					log.Print(err)
+				}
+				}
+				if message.Text=="我可以說什麼?" {
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你可以說 你是誰 今天吃甚麼 給我錢 你好 等等...")).Do();	
+				err != nil {
+					log.Print(err)
+				}
+				}
+
 				
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("你說"+message.Text+"我不明白")).Do();	
 				err != nil {
